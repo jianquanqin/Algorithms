006c65b15ea18a8ff86da50c18dd8a80cf5e5839
shiyivei <shiyivei@outlook.com>
2022-08-04 23:00:11 +0800

    day02

--- dataStructure/day1/day1.go
+++ dataStructure/day1/day1.go
@@ -25,6 +25,66 @@ func containsDuplicate(nums []int) bool {
 
 func maxSubArray(nums []int) int {
 
+	//初始数组
+	max := nums[0]
+     for i := 1; i < len(nums); i++ {
+
+        if nums[i] + nums[i-1] > nums[i] {
+            nums[i] += nums[i-1]
+        }
+        if nums[i] > max {
+            max = nums[i]
+        }
+    }
+    return max
+}
+
+//day2
+
+func twoSum(nums []int, target int) []int {
+
+	var res []int
+
+	for i:=0;i<len(nums);i++ {
+		for j:=i+1;j<len(nums);j++ {
+			if nums[j] + nums[i] == target {
+				return append(res, i,j)
+			}
+		}
+	}
+
+	return res
+}
+
+func merge(nums1 []int, m int, nums2 []int, n int)  {
+
+	for i:=0;i<n;i++ {
+
+		
+	}
 	
+
+}func merge(nums1 []int, m int, nums2 []int, n int)  {
+
+    sorted := make([]int, 0, m+n)
+    p1, p2 := 0, 0
+    for {
+        if p1 == m {
+            sorted = append(sorted, nums2[p2:]...)
+            break
+        }
+        if p2 == n {
+            sorted = append(sorted, nums1[p1:]...)
+            break
+        }
+        if nums1[p1] < nums2[p2] {
+            sorted = append(sorted, nums1[p1])
+            p1++
+        } else {
+            sorted = append(sorted, nums2[p2])
+            p2++
+        }
+    }
+    copy(nums1, sorted)
 
 }
